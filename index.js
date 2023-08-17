be31a76a143812ffa993ce0634db18ef35c1d5a3
paula <paulalaorga@gmail.com>
2023-08-17 21:36:33 +1000

    index

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let num = Math.random();
+let random = Math.ceil(num);
+console.log(random);
+
+let num3 = 6;
+let num4 = 2;
+let mod = num3 - num4;
+console.log(mod);
+
+let max = Math.max(1, 10, 20);
+console.log(max);
